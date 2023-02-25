a816c3d158c5724256f962e18cc42fbab4f3e287
syahfadel <syahfadel@gmail.com>
2023-02-25 20:30:33 +0700

    add menulis javascript

---
+++ javascript/P15_menulisJavaScript/Latihan.js
@@ -0,0 +1,7 @@
+alert("hello World2 dari external!");
+// var x; 
+x=10;
+// jika ingin js menampilkan tulisan pada console dapat menggunakan
+// console.log();
+console.log("Hello World");
+console.log("isi dari variabel x adalah " + x);
