03753e508b75ff6b6a69a3ca072d4dd7676be627
syahfadel <syahfadel@gmail.com>
2023-02-25 22:02:22 +0700

    add looping

---
+++ javascript/P17_pengulangan/perulangan.js
@@ -0,0 +1,15 @@
+/*
+pengulangan
+for
+while
+do while
+*/
+var i = 0;
+while(i<10){
+    console.log("while " + i);
+    i++;
+}
+
+for(var j = 0; j<10;j++){
+    console.log("for " + j);
+}
---
+++ javascript/latihan/angkot2.js
@@ -0,0 +1,12 @@
+var jmlAngkot = 10, 
+    noAngkot = 1, 
+    angkotBeroperasi = 6;
+
+while (noAngkot <= angkotBeroperasi){
+    console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.")
+    noAngkot++;
+}
+
+for (noAngkot; noAngkot<= jmlAngkot; noAngkot++){
+    console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi.")
+}
